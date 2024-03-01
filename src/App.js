be69a92c68e6b4d237797ed49e95ea2cf7fbe741
fabriciolopss <fabriciolopss@ufmg.br>
2024-02-29 23:55:41 -0300

    feat: stage files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      O teste é fazer um jogo da velha, com as funcionalidades básicas:
+      <br />
+      -Posicionar o X ou 0 <br />
+      -Atualizar o tabuleiro
+      <br />
+      -Definir quem é o vencedor
     </div>
   );
 }
